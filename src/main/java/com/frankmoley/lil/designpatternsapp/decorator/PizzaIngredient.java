df234d913c69d7b9522e67250b796f0aa221f641
Bartosz Abramowicz <abramovvicz@gmail.com>
2021-10-05 16:09:40 +0200

    add adapter and decorator pattern

---
+++ src/main/java/com/frankmoley/lil/designpatternsapp/decorator/PizzaIngredient.java
@@ -0,0 +1,7 @@
+package com.frankmoley.lil.designpatternsapp.decorator;
+
+import java.math.BigDecimal;
+
+public abstract class PizzaIngredient extends Pizza{
+    public abstract String getDescription();
+}
