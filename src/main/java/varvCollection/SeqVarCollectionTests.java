d9a94b41a740518cf3a5690052e3a421ed53d5b8
Bartosz <abramovvicz@gmail.com>
2021-11-22 13:30:13 +0100

    vavr collection

--- src/main/java/varvCollection/SeqVarCollectionTests.java
+++
@@ -1,9 +0,0 @@
-package varvCollection;
-
-import io.vavr.collection.CharSeq;
-
-public class SeqVarCollectionTests {
-    public static void main(String[] args) {
-        CharSeq.of("Vavr").filter(c->c != 'a').mkString(",");
-    }
-}
